ffb7569aae31327c5d4617f9968b99b4e49b9831
Mehmet Atakan Çavuşlu <mcavuslu15@ku.edu.tr>
2020-06-29 00:10:05 +0300

    Add CoinData model and implement JSON decoding.

---
+++ ByteCoinTracker/Model/CoinData.swift
@@ -0,0 +1,15 @@
+//
+//  CoinData.swift
+//  ByteCoinTracker
+//
+//  Created by Atakan Çavuşlu on 29.06.2020.
+//  Copyright © 2020 Atakan Çavuşlu. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    
+    let rate: Double
+    
+}
--- ByteCoinTracker/Model/CoinManager.swift
+++ ByteCoinTracker/Model/CoinManager.swift
@@ -8,7 +8,16 @@
 
 import Foundation
 
+protocol CoinManagerDelegate {
+    
+//    func didGetCoinPrice(_ coinManager: CoinManager, coin: CoinModel)
+    func didFailWithError(_ coinManager: CoinManager, error: Error)
+    
+}
+
 struct CoinManager {
+    
+    var delegate: CoinManagerDelegate?
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
     let apiKey = "DA5D619C-1DC7-4E71-8F68-FDCFCDA22446"
@@ -30,11 +39,28 @@ struct CoinManager {
             
             guard let safeData = data else { return }
             
-            // Handle the data
+            let coin = self.parseJSON(safeData)
             
         }
         
         task.resume()
+        
+    }
+    
+    func parseJSON(_ data: Data) -> Double? {
+        
+        let decoder = JSONDecoder()
+        
+        do {
+            
+            let decodedData = try decoder.decode(CoinData.self, from: data)
+            let rate = decodedData.rate
+            return rate
+            
+        } catch {
+            delegate?.didFailWithError(self, error: error)
+            return nil
+        }
         
     }
     
